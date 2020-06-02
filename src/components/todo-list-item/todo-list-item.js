62b0a069ec51099fb8036cba7c2145df74930115
DzmitrySudalenka <sudalenko@gmail.com>
2020-06-02 14:16:22 +0300

    todo done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,35 +4,43 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-    render() {
+  render() {
 
-        const { label, important = false } = this.props;
+    const { label, onDeleted,
+            onToggleImportant,
+            onToggleDone,
+            important, done } = this.props;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
-
-        return (
-            <span className="todo-list-item">
-              <span
-                  className="todo-list-item-label"
-                  style={style}>
-                {label}
-              </span>
-
-              <button type="button"
-                      className="btn btn-outline-success btn-sm float-right">
-                <i className="fa fa-exclamation" />
-              </button>
-
-              <button type="button"
-                      className="btn btn-outline-danger btn-sm float-right">
-                <i className="fa fa-trash-o" />
-              </button>
-            </span>
-        );
-
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
     }
 
+    if (important) {
+      classNames += ' important';
+    }
+
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={onToggleDone}>
+          {label}
+        </span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={onToggleImportant}>
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  };
 }
+
